3be706cdc517846f6878316778738ab6af4c7ed7
adam willetts <adam-willetts@hotmail.co.uk>
2023-02-10 21:32:18 +0000

    linked the image elements up to variables that change there src tag to photos of the recipe its linked too

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
 // html element selectors.
+let searchButton = document.querySelector(".btn-primary");
 let card1 = document.querySelector(".card-body1");
 let card2 = document.querySelector(".card-body2");
 let card3 = document.querySelector(".card-body3");
@@ -6,9 +7,10 @@ let h2Name1 = document.createElement("h2");
 let h2Name2 = document.createElement("h2");
 let h2Name3 = document.createElement("h2");
 let imgRecipe1 = document.querySelector(".card-img-top");
-let imgRecipe2 = document.querySelector(".card-img-top2");
-let imgRecipe3 = document.querySelector(".card-img-top3");
+let imgRecipe2 = document.querySelector(".cardTwo");
+let imgRecipe3 = document.querySelector(".cardThree");
 
+// headers for fetch request.
 const options = {
   method: "GET",
   headers: {
@@ -17,8 +19,7 @@ const options = {
   },
 };
 
-let recipeChK = document.getElementById("recipesCheck");
-let searchButton = document.querySelector(".btn-primary");
+// event listener added to search button that calls the function on click.
 searchButton.addEventListener("click", function (e) {
   e.preventDefault();
   let userInput = document.querySelector(".form-control").value;
@@ -29,19 +30,20 @@ searchButton.addEventListener("click", function (e) {
   )
     .then((response) => response.json())
     .then((response) => {
-      // let recipe = response.feed.content.ingredientLines
+// for loop that dynamically creates h2 elements and appends them to the cards.
       for (let i = 0; i < response.feed.length; i++) {
         console.log(response.feed);
+        // card one title and img src
         h2Name1.textContent = response.feed[0].display.displayName;
         card1.append(h2Name1);
         imgRecipe1.src = response.feed[0].display.images[0];
+        // card two title and img src
         h2Name2.textContent = response.feed[1].display.displayName;
         card2.append(h2Name2);
         imgRecipe2.src = response.feed[1].display.images[0];
+        // card 3 title and img
         h2Name3.textContent = response.feed[2].display.displayName;
-        console.log(response.feed[2].display.images[0])
         card3.append(h2Name3);
-        console.log(response.feed[3].display.images[0])
         imgRecipe3.src = response.feed[2].display.images[0];
       }
         // for (
