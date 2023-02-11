ee8d2a387f8ec13efc993d079881d9e3021a8b53
adam willetts <adam-willetts@hotmail.co.uk>
2023-02-11 15:43:58 +0000

    appended title and img too new cards

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,11 @@
 // html element selectors.
 let searchButton = document.querySelector(".btn-primary");
-let card1 = document.querySelector(".card-body1");
-let card2 = document.querySelector(".card-body2");
-let card3 = document.querySelector(".card-body3");
-let h2Name1 = document.createElement("h2");
-let h2Name2 = document.createElement("h2");
-let h2Name3 = document.createElement("h2");
-let imgRecipe1 = document.querySelector(".card-img-top");
-let imgRecipe2 = document.querySelector(".cardTwo");
-let imgRecipe3 = document.querySelector(".cardThree");
+let cardTitle1 = document.querySelector(".card-title1");
+let cardTitle2 = document.querySelector(".card-title2");
+let cardTitle3 = document.querySelector(".card-title3");
+let imgRecipe1 = document.querySelector(".imgRecipe1");
+let imgRecipe2 = document.querySelector(".imgRecipe2");
+let imgRecipe3 = document.querySelector(".imgRecipe3");
 
 // headers for fetch request.
 const options = {
@@ -30,28 +27,25 @@ searchButton.addEventListener("click", function (e) {
   )
     .then((response) => response.json())
     .then((response) => {
-// for loop that dynamically creates h2 elements and appends them to the cards.
+      // for loop that dynamically creates h2 elements and appends them to the cards.
       for (let i = 0; i < response.feed.length; i++) {
         console.log(response.feed);
         // card one title and img src
-        h2Name1.textContent = response.feed[0].display.displayName;
-        card1.append(h2Name1);
+        cardTitle1.textContent = response.feed[0].display.displayName;
         imgRecipe1.src = response.feed[0].display.images[0];
         // card two title and img src
-        h2Name2.textContent = response.feed[1].display.displayName;
-        card2.append(h2Name2);
+        cardTitle2.textContent = response.feed[1].display.displayName;
         imgRecipe2.src = response.feed[1].display.images[0];
-        // card 3 title and img
-        h2Name3.textContent = response.feed[2].display.displayName;
-        card3.append(h2Name3);
+        // card three title and img src
+        cardTitle3.textContent = response.feed[2].display.displayName;
         imgRecipe3.src = response.feed[2].display.images[0];
       }
-        // for (
-        //   let j = 0;
-        //   j < response.feed[i].content.ingredientLines.length;
-        //   j++
-        // ) {
-        //   // console.log(response.feed[i].content.ingredientLines[j].wholeLine)
-        // }
-      }
-);})
+      // for (
+      //   let j = 0;
+      //   j < response.feed[i].content.ingredientLines.length;
+      //   j++
+      // ) {
+      //   // console.log(response.feed[i].content.ingredientLines[j].wholeLine)
+      // }
+    });
+});
