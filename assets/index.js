a5d10db24e11df39f1bb379f1bf0e511a34bf3c0
samuel_tuki <smauel_tuki@yahoo.com>
2023-02-15 21:04:51 +0000

    clear content

--- assets/index.js
+++ assets/index.js
@@ -8,9 +8,10 @@ let cardBody1 = document.querySelector(".card1");
 let p2El = document.createElement("p");
 let pEl = document.createElement("p");
 let emptyString = document.querySelector(".pClass");
+let ingredientEl = document.querySelector(".instructions");
 
 // api key
-const apiKey = "cd8d4115f8c64fd0859f8a90e0d57eb7";
+const apiKey = "1fce12db04a04be5be21bfb6692f4d2b";
 
 // event listener that fires off fetch requests on click.
 searchButton.addEventListener("click", function (e) {
@@ -40,6 +41,20 @@ searchButton.addEventListener("click", function (e) {
       )
         .then((response) => response.json())
         .then((info) => {
+          cardTitle1.textContent = ""
+          p2El.innerHTML = ""
+          ingredientEl.textContent = ""
+           // remove hide class to show card on search.
+           hideClass.classList.remove("hide");
+           // displays title
+           cardTitle1.textContent = info.title;
+           // changes image src to image of recipe
+           imgRecipe1.src = info.image;
+           // appends list of instructions
+           p2El.innerHTML = info.instructions;
+           
+           cardBody1.append(p2El);
+
           // for loop that dynamicaly creates p elements and appends each ingredient of array to page
           for (let i = 0; i < info.extendedIngredients.length; i++) {
             pEl = document.createElement("p");
@@ -47,27 +62,27 @@ searchButton.addEventListener("click", function (e) {
             let emptyString = document.querySelector(".pClass");
             // emptyString.textContent = "";
             pEl.textContent = info.extendedIngredients[i].original;
-            cardBody1.append(pEl);
+            ingredientEl.append(pEl);
+            
           }
-          // remove hide class to show card on search.
-          hideClass.classList.remove("hide");
-          // displays title
-          cardTitle1.textContent = info.title;
-          // changes image src to image of recipe
-          imgRecipe1.src = info.image;
-          // appends list of instructions
-          p2El.innerHTML = info.instructions;
-          cardBody1.append(p2El);
+         
+       
+
         });
+
+      
     });
+
+    
 });
 
-// function translateText(){
+// function translateText(translatedText){
+
 
 //   const encodedParams = new URLSearchParams();
 //   encodedParams.append("source", "en");
 //   encodedParams.append("target", "es");
-//   encodedParams.append("q", params);
+//   encodedParams.append("q",  translatedText);
 
 //   const options = {
 //     method: 'POST',
@@ -85,7 +100,8 @@ searchButton.addEventListener("click", function (e) {
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
 
-// translateText("info.title")
+
+
+
 
 // }
-
